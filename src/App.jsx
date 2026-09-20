ab6f5e8be376c51ac752a9d294bc70bae8f2a4ac
szoubinedev <zoubinesoulaimane@gmail.com>
2026-09-20 18:16:57 +0000

    initilize repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-rose-300 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-black-100">
+        Tailwind is working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
